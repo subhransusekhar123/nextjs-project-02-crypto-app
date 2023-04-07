bf76feb6cfab9021912f23017df852fbf12fee9b
unknown <subhransusekharsahu29@gmail.com>
2023-04-07 20:18:01 +0530

    done

--- pages/coin/[id]/index.js
+++ pages/coin/[id]/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const index = () => {
+const index = ({coinsData}) => {
   return (
     <div>Hello , shankar</div>
   )
@@ -9,7 +9,7 @@ const index = () => {
 export default index;
 
 export const getServerSideProps = async (context) =>{
-  console.log(context.)
+  console.log(context.params.id);
   const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
   const coinsData = await res.json();
   return{
